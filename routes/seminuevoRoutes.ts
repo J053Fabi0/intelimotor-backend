f66e3c6fc4fb02d0f2fe7bfe63fef74cb57d9aa7
Amiko <senorbinario@tuta.io>
2022-09-11 13:37:45 -0500

    Timeout a POST /seminuevo de 3 minutos

--- routes/seminuevoRoutes.ts
+++ routes/seminuevoRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import timeout from "connect-timeout";
 import * as s from "../schemas/seminuevoSchema";
 import * as c from "../controllers/seminuevoController";
 
 const seminuevoRoutes = Router();
 
-seminuevoRoutes.post("/seminuevo", s.postSeminuevo, c.postSeminuevo);
+seminuevoRoutes.post("/seminuevo", timeout("3m"), s.postSeminuevo, c.postSeminuevo);
 
 export default seminuevoRoutes;
